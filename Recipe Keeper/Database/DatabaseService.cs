53e9a2538d758ea8cfad48779b6d47f4bb19cae9
Alex <ari1268@wgu.edu>
2025-03-01 22:39:33 -0600

    Added the default .NET MAUI application structure.

---
+++ Recipe Keeper/Database/DatabaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Recipe_Keeper.Database
+{
+    internal class DatabaseService
+    {
+        //The SQLite connection object.
+        private readonly SQLiteAsyncConnection dbConnection;
+
+
+
+    }
+}
---
+++ Recipe Keeper/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Recipe_Keeper.Database;
+
+namespace Recipe_Keeper
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            //Add database services to the DI container.
+            builder.Services.AddSingleton(s =>
+            {
+                string dbPath = Path.Combine(FileSystem.AppDataDirectory, "rkmaDB.db3");
+                return new DatabaseService(dbPath);
+            });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
