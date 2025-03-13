78ce985adeb6b1529f36ef7a222299ccccb2cdec
Alex <ari1268@wgu.edu>
2025-03-13 14:12:33 -0500

    Added Count option to units list.

--- Recipe Keeper/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Recipe Keeper.AssemblyInfo.cs
+++ Recipe Keeper/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Recipe Keeper.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe Keeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da3909406f644bb15c6e713e60cedc40584f741e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8a612c3489f3417dcb910ea3710326fc39b3bd42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe Keeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe Keeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
