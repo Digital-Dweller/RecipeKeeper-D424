0390c04e9298b04e5526cf9c9fa90f8a1e8c3afc
Alex Richards <alex.h.richards@outlook.com>
2025-03-20 21:14:13 -0500

    Adjusted color formatting for picker objects to resolve issues with dark mode. Reduced ingredient and direction control sizes to fit narrow screens better.

--- obj/Release/net9.0-android/Recipe Keeper.AssemblyInfo.cs
+++ obj/Release/net9.0-android/Recipe Keeper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe Keeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ec91d8dfcac71022775df947b28fe80489f8e546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d259655b8bd3462100183d017bd7c16ac16f6411")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe Keeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe Keeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
